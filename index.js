79029bbf4bab61e6ce2d12a94c6f2c7e8429e0c0
Rod <aaronrodolfo2022@gmail.com>
2026-04-01 18:15:14 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//a piece of work in javascript.
+
+
+let x = "RandomXDD";
+
+document.getElementById(`container`).textContent = (`Your name is ${x}!`);
+
+let y;
+y = 16;
+
+document.getElementById(`p1`).textContent = (`Your age is ${y} years old!`);
+
+let z = true;
+
+document.getElementById(`myH4`).textContent = (`You still have a balance in fee: ${z}`);
+
+
+let a = "ushaufhsadiusahduishuadhuisahdiuhaa";
+
+document.getElementById(`paragraph`).textContent = (`this is a paragraph: ${a}, bye!`);
